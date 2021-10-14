68198ed481fa38cf64a3b86a778b0d204881ba03
Adrian Mysliwiec <mysliwiec.a02@htlwienwest.at>
2021-10-14 22:42:32 +0200

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-<!-- Aufgabe 2 --Anordnung spielt keine rolle -->
-  <div id="app">
+  <!-- Aufgabe 1 -->
+  <!-- <div style="display:flex">
+    <Sheet bg-color="#3498db" color="white" width="250" height="300">
+      <h3>Memo to myself</h3>
+      <p>Always prepare for PLFs in time!</p>
+      <img src="info.svg" width="80px" />
+    </Sheet>
+    <Sheet bg-color="#8e44ad" color="yellow" width="400" height="300">
+      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
+      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
+      <p>Wish is was there!</p>
+      <span style="font-style: italic">E. Wahn</span>
+    </Sheet>
+  </div> -->
+
+  <!-- Aufgabe 2 --Nein,Anordnung spielt keine rolle -->
+  <!-- <div id="app">
     <v-data-table
       :headers="headers"
       :items="desserts"
@@ -28,6 +43,26 @@
         ></v-text-field>
       </template>
     </v-data-table>
+  </div> -->
+
+  <!-- AUFGABE 3 -->
+  <div id="app">
+    <DataTable :items="desserts" :headers="headers">
+      <template v-slot:header>
+        <h2>Watch What You Eat!</h2>
+      </template>
+      <template v-slot:footer> Created 2021 by Robert Baumgartner </template>
+      <template v-slot:bodyappend>
+        <tr class="last-row"> 
+          <td >Total</td>
+          <td>{{ totalCalories }}</td>
+          <td>{{ totalFat }}</td>
+          <td>{{ totalCarbs }}</td>
+          <td>{{ totalProtein }}</td>
+          <td></td>
+        </tr>
+      </template>
+    </DataTable>
   </div>
 </template>
 
@@ -35,6 +70,7 @@
 // import AppButton from '@/components/AppButton.vue';
 // // import Sheet from '@/components/Sheet.vue';
 // import AppLayout from '@/components/AppLayout.vue';
+import DataTable from '@/components/DataTable.vue';
 
 export default {
   name: 'App',
@@ -42,9 +78,79 @@ export default {
     // AppButton,
     // // Sheet,
     // AppLayout,
+     DataTable,
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (g)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        
+      ],
+    };
+  },
+  computed: {
+    totalCalories() {
+      return this.desserts.map((el) => el.calories).reduce((a, b) => a + b);
+    },
+    totalFat() {
+      return this.desserts.map((el) => el.fat).reduce((a, b) => a + b);
+    },
+    totalCarbs() {
+      return this.desserts.map((el) => el.carbs).reduce((a, b) => a + b);
+    },
+    totalProtein() {
+      return this.desserts.map((el) => el.protein).reduce((a, b) => a + b);
+    },
+  },
 };
 </script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="data-table">
+    <h1><slot name="header"></slot></h1>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="head of headers" :key="head">{{ head.text }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="it of items" :key="it">
+          <td>{{ it.name }}</td>
+          <td>{{ it.calories }}</td>
+          <td>{{ it.fat }}</td>
+          <td>{{ it.carbs }}</td>
+          <td>{{ it.protein }}</td>
+          <td>{{ it.iron }}</td>
+        </tr>
+        <slot name="bodyappend"></slot>
+      </tbody>
+    </table>
+    <footer><slot name="footer"></slot></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+    },
+    headers: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.data-table {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 40px;
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.data-table td,
+th {
+  text-align: left;
+}
+.data-table footer {
+  margin: 30px 0 30px 0;
+  font-style: italic;
+}
+.data-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.data-table th,
+.data-table td {
+  padding: 12px 15px;
+}
+.data-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.data-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+</style>
